57e86ea0ff0fc1baee732a104f104dbfbbaab7d9
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-01-10 16:01:18 -0800

    Adds email and name

--- variables.js
+++ variables.js
@@ -1,5 +1,12 @@
 var email = "chamberlainuxdesign@gmail.com"
-var name = "nathan chamberlain"
+var fullName = "nathan chamberlain"
 
 var age = 30
 var yearBorn = 1992
+
+email = "nchamb22@turing.edu"
+fullName = "nate chambo"
+
+console.log(email);
+console.log(fullName);
+
